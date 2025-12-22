fa4e294906a6f289345c944251eb2a8c88667d27
Roland <1124997@hr.nl>
2025-12-23 00:36:08 +0100

    refactor: remove unused button elements from filter component

--- app/components/utils/button/filter.vue
+++ app/components/utils/button/filter.vue
@@ -70,18 +70,3 @@
 		return isActive ? colors[color]?.active : colors[color]?.inactive;
 	};
 </script>
-
-<!-- <button type="button" @click="callback('all')" :class="['flex items-center justify-center gap-2 px-4 py-[0.60rem] md:py-2 text-sm font-medium transition-colors duration-200 border rounded-lg outline-none w-fit focus:outline-none focus:ring-2', filter === 'all' ? 'bg-neutral-100 text-neutral-800 border-neutral-400 focus:ring-neutral-400' : 'text-gray-700 bg-white border-gray-300 hover:bg-neutral-50 hover:text-neutral-600 focus:text-neutral-600 focus:border-neutral-500 hover:border-neutral-500 focus:ring-neutral-400']" aria-label="Toon alle berichten">
-						<icon name="akar-icons:filter" class="w-4 h-4" aria-hidden="true" />
-						<span class="hidden md:flex">Alles</span>
-					</button>
-
-					<button type="button" @click="callback('gelezen')" :class="['flex items-center justify-center flex-1 gap-2 px-4 py-2 text-sm font-medium transition-colors duration-200 border rounded-lg outline-none focus:outline-none focus:ring-2', filter === 'gelezen' ? 'bg-blue-100 text-blue-800 border-blue-400 focus:ring-blue-300' : 'text-gray-700 bg-white border-gray-300 hover:bg-blue-50 hover:text-blue-600 focus:text-blue-600 focus:border-blue-500 hover:border-blue-500 focus:ring-blue-300']" aria-label="Zoek gelezen berichten">
-						<icon name="akar-icons:open-envelope" class="w-4 h-4" aria-hidden="true" />
-						<span> Gelezen </span>
-					</button>
-
-					<button type="button" @click="callback('ongelezen')" :class="['flex items-center justify-center flex-1 gap-2 px-4 py-2 text-sm font-medium transition-colors duration-200 border rounded-lg outline-none focus:outline-none focus:ring-2', filter === 'ongelezen' ? 'bg-red-100 text-red-800 border-red-400 focus:ring-red-300' : 'text-gray-700 bg-white border-gray-300 hover:bg-red-50 focus:text-red-600 hover:text-red-600 focus:border-red-500 hover:border-red-500 focus:ring-red-300']" :aria-pressed="filter === 'ongelezen'" aria-label="Zoek ongelezen berichten">
-						<icon name="akar-icons:envelope" class="w-4 h-4" aria-hidden="true" />
-						<span> Ongelezen </span>
-                    </button> -->
